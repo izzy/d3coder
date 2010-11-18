a910df61a4d9e0cb6abb521ebf68f3e9a35cf742
Izzy <360964+izzy@users.noreply.github.com>
2010-11-18 16:46:45 +0100

    Fixed the unserialize output(printet only string 'object')

--- js/D3.js
+++ js/D3.js
@@ -1002,7 +1002,7 @@ var D3 =
 	        return [dtype, dataoffset - offset, typeconvert(readdata)];
 	    };
 	    
-	    return _unserialize((data+''), 0)[2];
+	    return D3.var_dump(_unserialize((data+''), 0)[2]);
 	},
 	utf8_encode: function ( argString ) {
 	    var string = (argString+''); // .replace(/\r\n/g, "\n").replace(/\r/g, "\n");
@@ -1064,6 +1064,70 @@ var D3 =
 	    return tmp_arr.join('');
 	},
 
+	/**
+	 * Concatenates the values of a variable into an easily readable string
+	 * by Matt Hackett [scriptnode.com]
+	 * @param {Object} x The variable to debug
+	 * @param {Number} max The maximum number of recursions allowed (keep low, around 5 for HTML elements to prevent errors) [default: 10]
+	 * @param {String} sep The separator to use between [default: a single space ' ']
+	 * @param {Number} l The current level deep (amount of recursion). Do not use this parameter: it's for the function's own use
+	 */
+	var_dump: function (x, max, sep, l) {
+
+	    l = l || 0;
+	    max = max || 10;
+	    sep = sep || ' ';
+
+	    if (l > max) {
+	        return "[WARNING: Too much recursion]\n";
+	    }
+
+	    var
+	        i,
+	        r = '',
+	        t = typeof x,
+	        tab = '';
+
+	    if (x === null) {
+	        r += "(null)\n";
+	    } else if (t == 'object') {
+
+	        l++;
+
+	        for (i = 0; i < l; i++) {
+	            tab += sep;
+	        }
+
+	        if (x && x.length) {
+	            t = 'array';
+	        }
+
+	        r += '(' + t + ") :\n";
+
+	        for (i in x) {
+	            try {
+	                r += tab + '[' + i + '] : ' + D3.var_dump(x[i], max, sep, (l + 1));
+	            } catch(e) {
+	                return "[ERROR: " + e + "]\n";
+	            }
+	        }
+
+	    } else {
+
+	        if (t == 'string') {
+	            if (x == '') {
+	                x = '(empty)';
+	            }
+	        }
+
+	        r += '(' + t + ') ' + x + "\n";
+
+	    }
+
+	    return r;
+
+	},
+	
 	menus: [],
 	
 	menuLoadTime: 1000,
